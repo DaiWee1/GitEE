1e7543926c957f2dca31371e25d43019151fcdec
DDW <1103104137@gm.kuas.edu.tw>
2016-10-26 11:20:04 +0800

    3

--- ex1.c
+++ ex1.c
@@ -7,7 +7,7 @@ main()
       for (i = 1; i <= 9; i++)
       {
           for (j = 1; j <= 9; j++)
-              printf("%d*%d=%2d", i, j, i*j);
+              printf("%d*%d=%5d", i, j, i*j);
           printf("\n");
       }
       system("pause");
